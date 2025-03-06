f14abcad012a7eb28c5063a0fcb4e03cfaeaad23
Dcavise <cavise@primer.com>
2025-03-06 16:25:38 -0600

    Add client.js and index.js to fix Vercel build issues

---
+++ src/integrations/supabase/client.js
@@ -0,0 +1,2 @@
+// Re-export the contents of client.ts
+export * from './client.ts'; 
---
+++ src/integrations/supabase/index.js
@@ -0,0 +1,3 @@
+// Re-export the contents of client.ts and admin-client.ts
+export * from './client.ts';
+export * from './admin-client.ts'; 
